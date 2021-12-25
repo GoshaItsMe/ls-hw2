371fa121079532a3633572ab3fbbda128c438d49
mukhammad <23@.ru>
2021-12-25 06:06:42 -0600

    end

---
+++ script.js
@@ -0,0 +1,82 @@
+var num = +prompt('Введите цыфру чтобы узнать время от 0 до 24');
+
+switch(num) {
+    case 0:
+        alert('12 часов ночи');
+        break;
+    case 1:
+        alert('Час утра');
+        brake;
+    case 2:
+        alert('2 часа утра');
+        brake;
+    case 3:
+        alert('3 часа утра');
+        brake;
+    case 4:
+        alert('4 часа утра');
+        brake;
+    case 5:
+        alert('5 часов утра');
+        brake;
+    case 6:
+        alert('6 часов утра');
+        brake;
+    case 7:
+        alert('7 часов утра');
+        brake;
+    case 8:
+        alert('8 часов утра');
+        brake;
+    case 9:
+        alert('9 часов утра');
+        brake;
+    case 10:
+        alert('10 часов утра');
+        brake;
+    case 11:
+        alert('11 часов дня');
+        brake;
+    case 12:
+        alert('12 часов дня');
+        brake;
+    case 13:
+        alert('час дня');
+        brake;
+    case 14:
+        alert('2 часа дня');
+        brake;
+    case 15:
+        alert('3 часа дня');
+        brake;
+    case 16:
+        alert('4 часа дня');
+        brake;
+    case 17:
+        alert('5 часов дня');
+        brake;
+    case 18:
+        alert('6 часов вечера');
+        brake;
+    case 19:
+        alert('7 часво вечера');
+        brake;
+    case 20:
+        alert('8 часов вечера');
+        brake;
+    case 21:
+        alert('9 часов ночи');
+        brake;
+    case 22:
+        alert('10 часов ночи');
+        brake;
+    case 23:
+        alert('11 часов ночи');
+        brake;
+    case 24:
+        alert('А чё не спищь');
+        brake;
+    default:
+        alert('Вы ввели не правельно');
+        brake;
+}
